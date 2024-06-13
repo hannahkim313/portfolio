7dc0f9df1b9b633d146196e14c4b2f214acaffb0
Hannah Kim <67137839+hannahkim313@users.noreply.github.com>
2024-06-12 17:46:25 -0700

    Edit description in the about section and add CSS styles to the provided link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,6 +16,21 @@ const About = () => (
           both user- and developer-friendly and ultimately contribute to
           software development focused on dynamism and scalability.
         </p>
+        <p>
+          Although my skill set mainly lies in front-end languages and
+          frameworks such as HTML, CSS, JavaScript, and React, I am learning to
+          work with backend languages and technologies like NodeJS, Express,
+          MongoDB, and Mongoose. My{' '}
+          <a href="https://github.com/hannahkim313/inventory-app">
+            inventory application
+          </a>{' '}
+          highlights my introductory knowledge of these concepts and tools.
+          Moving forward, I plan to learn about advanced topics such as
+          authentication, APIs, and testing to gain a better understanding of
+          backend development. My goal is to have a more holistic view of a
+          project to become a versatile front-end developer in collaborative
+          settings.
+        </p>
       </div>
       <img
         className={styles.authorImg}
