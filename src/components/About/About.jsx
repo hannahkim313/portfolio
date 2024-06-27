d96db2503e80aca58038b266595dee7c796f6cc7
Hannah Kim <67137839+hannahkim313@users.noreply.github.com>
2024-06-27 11:18:19 -0700

    Add skills to the About component and edit the author's description

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => (
     <section className={styles.about}>
       <div className={styles.left}>
         <h2 id="about">About the Developer</h2>
-        <h3 className={styles.subheading}>
+        <h3>
           Hello! I&apos;m Hannah, a front-end web developer looking to
           transition into a role focused on JavaScript and React.
         </h3>
@@ -20,18 +20,37 @@ const About = () => (
         <p>
           Although my skill set mainly lies in front-end languages and
           frameworks such as HTML, CSS, JavaScript, and React, I am learning to
-          work with backend languages and technologies like NodeJS, Express,
-          MongoDB, and Mongoose. My{' '}
-          <a href="https://github.com/hannahkim313/inventory-app">
-            inventory application
-          </a>{' '}
-          highlights my introductory knowledge of these concepts and tools.
-          Moving forward, I plan to learn about advanced topics such as
-          authentication, APIs, and testing to gain a better understanding of
-          backend development. My goal is to have a more holistic view of a
-          project to become a versatile front-end developer in collaborative
-          settings.
+          work with backend languages and technologies like NodeJS, ExpressJS,
+          and MongoDB. Moving forward, I plan to learn about advanced topics
+          such as authentication, APIs, and testing to gain a better
+          understanding of backend development. My goal is to have a more
+          holistic view of a project to become a versatile front-end developer
+          in collaborative settings.
         </p>
+        <p>
+          <strong>Web Development Skills:</strong>
+        </p>
+        <ul className={styles.skills}>
+          <li>JavaScript</li>
+          <li>React.js</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>RESTful APIs</li>
+          <li>Jest</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>MongoDB</li>
+        </ul>
+        <p>
+          <strong>Web Technologies:</strong>
+        </p>
+        <ul className={styles.skills}>
+          <li>Git</li>
+          <li>NPM</li>
+          <li>GitHub</li>
+          <li>Visual Studio Code</li>
+          <li>Chrome DevTools</li>
+        </ul>
       </div>
       <img
         className={styles.authorImg}
